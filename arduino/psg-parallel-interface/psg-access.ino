285031dcdd48aa3ac2e2ec0c237553ce310744e1
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2021-12-18 23:16:33 +0200

    More detailed timings + shortest timings for the emulator

--- arduino/psg-parallel-interface/psg-access.ino
+++ arduino/psg-parallel-interface/psg-access.ino
@@ -24,10 +24,12 @@
 
 // timing delays (nano seconds)
 #define PSG_Delay(ns) _delay_us(0.001f * (ns))
-#define tAS 300
-#define tAH 100
-#define tDW 500
-#define tDA 200
+#define tAS 800 // min 400 ns by datasheet
+#define tAH 100 // min 100 ns by datasheet
+#define tDW 500 // min 500 ns by datasheet
+#define tDH 100 // min 100 ns by datasheet
+#define tDA 500 // max 500 ns by datasheet
+#define tTS 100 // max 200 ns by datasheet
 
 // MCU clock to PSG clock deviders
 #define CLK_177_MHz 9
@@ -36,6 +38,12 @@
 // -----------------------------------------------------------------------------
 // Low Level Access
 // -----------------------------------------------------------------------------
+
+void PSG_GetDataBus(byte& data)
+{
+    // get bata bits from input ports
+    data = (LSB_PIN & LSB_MASK) | (MSB_PIN & MSB_MASK);
+}
 
 void PSG_SetDataBus(byte data)
 {
@@ -48,16 +56,8 @@ void PSG_SetDataBus(byte data)
     MSB_PORT = (MSB_PORT & ~MSB_MASK) | (data & MSB_MASK);
 }
 
-void PSG_GetDataBus(byte& data)
+void PSG_ReleaseDataBus()
 {
-    // get bata bits from input ports
-    data = (LSB_PIN & LSB_MASK) | (MSB_PIN & MSB_MASK);
-}
-
-void PSG_Inactive()
-{
-    BUS_PORT &= ~(1 << PIN_BDIR | 1 << PIN_BC1);
-
     // setup ports to input
     LSB_DDR &= ~LSB_MASK;
     MSB_DDR &= ~MSB_MASK;
@@ -67,6 +67,11 @@ void PSG_Inactive()
     MSB_PORT |= MSB_MASK;
 }
 
+void PSG_Inactive()
+{
+    BUS_PORT &= ~(1 << PIN_BDIR | 1 << PIN_BC1);
+}
+
 void PSG_Address(byte reg)
 {
     PSG_SetDataBus(reg);
@@ -74,6 +79,7 @@ void PSG_Address(byte reg)
     PSG_Delay(tAS);
     PSG_Inactive();
     PSG_Delay(tAH);
+    PSG_ReleaseDataBus();
 }
 
 void PSG_Write(byte data)
@@ -82,6 +88,8 @@ void PSG_Write(byte data)
     BUS_PORT |= (1 << PIN_BDIR);
     PSG_Delay(tDW);
     PSG_Inactive();
+    PSG_Delay(tDH);
+    PSG_ReleaseDataBus();
 }
 
 void PSG_Read(byte& data)
@@ -90,6 +98,7 @@ void PSG_Read(byte& data)
     PSG_Delay(tDA);
     PSG_GetDataBus(data);
     PSG_Inactive();
+    PSG_Delay(tTS);
 }
 
 // -----------------------------------------------------------------------------
@@ -105,12 +114,11 @@ void PSG_Init()
     OCR2A  = (CLK_177_MHz - 1);
     OCR2B  = (CLK_177_MHz / 2); 
 
-    // setup control pins for output
+    // setup control and data bus
     BUS_DDR |= (1 << PIN_BDIR);
     BUS_DDR |= (1 << PIN_BC1);
-
-    // start with inactive mode
     PSG_Inactive();
+    PSG_ReleaseDataBus();
 }
 
 void PSG_Send(byte reg, byte data)
